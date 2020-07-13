11386f62a79448d1522830fd6dd018f966cdaf7f
Dong Yuwei <newdongyuwei@gmail.com>
2020-07-13 16:21:09 +0800

    add new unit test: create contact then display it

--- src/pages/ContactForm/index.vue
+++ src/pages/ContactForm/index.vue
@@ -1,44 +1,52 @@
 <template>
-  <div class="contact-form">
+  <div class="contact-form" data-testid="contact-form">
     <h3>Contact Form</h3>
     <div class="form-control">
       <span class="label">姓名</span>
-      <input type="text" v-model="name" />
+      <input type="text" v-model="name" data-testid="name" />
     </div>
     <div class="form-control">
       <span class="label">电话</span>
       <div style="display: inline-block;">
-        <select v-model="telphone.type">
+        <select v-model="telphone.type" data-testid="telphone.type">
           <option value="home">home</option>
           <option value="work">work</option>
         </select>
-        <input v-model.number="telphone.mobile" type="number" />
+        <input v-model.number="telphone.mobile" type="number" data-testid="telphone.mobile" />
       </div>
     </div>
     <div class="form-control">
       <span class="label">性别</span>
       <label>
-        <input type="radio" v-model="gender" value="男" />
+        <input type="radio" v-model="gender" value="男" data-testid="gender.male" />
         <span>男</span>
       </label>
       <label>
-        <input type="radio" v-model="gender" value="女" />
+        <input type="radio" v-model="gender" value="女" data-testid="gender.female" />
         <span>女</span>
       </label>
-      
     </div>
     <div class="form-control">
       <span class="label">标签</span>
-      <label><input type="checkbox" v-model="tags" value="中介" />中介</label>
-      <label><input type="checkbox" v-model="tags" value="外卖" />外卖</label>
-      <label><input type="checkbox" v-model="tags" value="诈骗" />诈骗</label>
+      <label>
+        <input type="checkbox" v-model="tags" value="中介" data-testid="tags.agent" />中介
+      </label>
+      <label>
+        <input type="checkbox" v-model="tags" value="外卖" data-testid="tags.takeout" />外卖
+      </label>
+      <label>
+        <input type="checkbox" v-model="tags" value="诈骗" data-testid="tags.cheat" />诈骗
+      </label>
     </div>
     <div class="form-control">
       <span class="label" style="vertical-align: top;">地址</span>
-      <textarea v-model="address"></textarea>
+      <textarea v-model="address" name="address" data-testid="address"></textarea>
     </div>
     <div class="form-control">
-      <button @click="onSubmit">{{ editMode ? '修改Contact' : '添加Contact'}}</button>
+      <button
+        @click="onSubmit"
+        data-testid="create-contact"
+      >{{ editMode ? '修改Contact' : '添加Contact'}}</button>
     </div>
   </div>
 </template>
@@ -47,13 +55,16 @@ export default {
   props: {
     contact: {
       type: Object
-    } 
+    }
   },
   data() {
-    const contact = this.$props.contact || this.getDefaultFields()
+    const contact = this.$props.contact || this.getDefaultFields();
     return {
       name: contact.name,
-      telphone: { type: contact.telphone.type, mobile: contact.telphone.mobile },
+      telphone: {
+        type: contact.telphone.type,
+        mobile: contact.telphone.mobile
+      },
       gender: contact.gender,
       tags: contact.tags,
       address: contact.address,
@@ -61,57 +72,61 @@ export default {
     };
   },
   methods: {
-    getDefaultFields(){
-      return { 
-        telphone: { type: "home", mobile: "" }, tags: [], gender: '男'
-      }
+    getDefaultFields() {
+      return {
+        telphone: { type: "home", mobile: "" },
+        tags: [],
+        gender: "男"
+      };
     },
     resetFields() {
-      this.name = ''
-      this.telphone = { type: "home", mobile: "" }
-      this.tags = []
-      this.address = ''
+      this.name = "";
+      this.telphone = { type: "home", mobile: "" };
+      this.tags = [];
+      this.address = "";
     },
     onSubmit() {
-      const { submit } = this.$listeners
-      const fields = this.getFormFields()
-      if(!fields.name || !fields.telphone.mobile) {
-        alert('please type name or phone')
-        return
+      const { submit } = this.$listeners;
+      const fields = this.getFormFields();
+      if (!fields.name || !fields.telphone.mobile) {
+        alert("please type name or phone");
+        return;
       }
-      submit && submit(this.getFormFields())
-      this.resetFields()
+      submit && submit(this.getFormFields());
+      this.resetFields();
     },
-    getFormFields(){
+    getFormFields() {
       return {
         name: this.name,
         telphone: { type: this.telphone.type, mobile: this.telphone.mobile },
         gender: this.gender,
         tags: this.tags,
         address: this.address
-      }
+      };
     }
   }
 };
 </script>
 <style lang="less">
-  .contact-form{
-    .form-control{
+.contact-form {
+  .form-control {
+    display: inline-block;
+    width: 400px;
+    margin-bottom: 10px;
+    vertical-align: top;
+    .label {
       display: inline-block;
-      width: 400px;
-      margin-bottom: 10px;
-      vertical-align: top;
-      .label{
-        display: inline-block;
-        width: 60px;
-        line-height: 32px;
-        height: 32px;
-      }
-      input[type=text], input[type=number], select{
-        box-sizing: border-box;
-        line-height: 30px;
-        height: 32px;
-      }
+      width: 60px;
+      line-height: 32px;
+      height: 32px;
+    }
+    input[type="text"],
+    input[type="number"],
+    select {
+      box-sizing: border-box;
+      line-height: 30px;
+      height: 32px;
     }
   }
+}
 </style>
--- tests/unit/contacts.spec.js
+++ tests/unit/contacts.spec.js
@@ -1,9 +1,9 @@
-import { render } from "@testing-library/vue";
+import { render, fireEvent } from "@testing-library/vue";
 import ContactList from "../../src/pages/ContactList/index.vue";
 
 const nock = require("nock");
 
-beforeAll(() => {
+beforeEach(() => {
   nock("http://api.contacts.cn")
     .get("/api/contacts")
     .reply(200, [
@@ -19,10 +19,54 @@ beforeAll(() => {
 });
 
 it("should load contacts and display them", async done => {
-  const { findByText, getByText, debug } = render(ContactList);
+  const { findByText, getByText } = render(ContactList);
   // debug();
   await findByText("youyuxi");
-  debug();
+  // debug();
   expect(getByText("home 15502980060")).not.toBeNull();
+  done();
+});
+
+it("should fill contact form and create new contact", async done => {
+  const formValues = {
+    address: "xi'an",
+    gender: "女",
+    name: "dyw",
+    tags: ["中介", "外卖"],
+    telphone: { mobile: 15502980080, type: "work" }
+  };
+  nock("http://api.contacts.cn")
+    .post("/api/contacts")
+    .reply((uri, requestBody) => {
+      // eslint-disable-next-line no-unused-vars
+      const { id, ...body } = requestBody;
+      expect(body).toEqual(formValues);
+
+      nock("http://api.contacts.cn")
+        .get("/api/contacts")
+        .reply(200, [requestBody]);
+
+      return [201, requestBody];
+    });
+
+  const { findByText, getByTestId, getByText } = render(ContactList);
+  await fireEvent.update(getByTestId("name"), formValues.name);
+  await fireEvent.update(getByTestId("address"), formValues.address);
+  await fireEvent.update(
+    getByTestId("telphone.mobile"),
+    formValues.telphone.mobile
+  );
+  await fireEvent.update(
+    getByTestId("telphone.type"),
+    formValues.telphone.type
+  );
+
+  fireEvent.click(getByTestId("gender.female"), formValues.gender);
+  fireEvent.click(getByTestId("tags.agent"));
+  fireEvent.click(getByTestId("tags.takeout"));
+
+  fireEvent.click(getByTestId("create-contact"));
+  await findByText("dyw"); // wait for the `/api/contacts` api request
+  expect(getByText("work 15502980080")).not.toBeNull();
   done();
 });
