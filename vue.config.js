ecaec193d90851ef5fa5df54b6000566d2eee69c
zengge <gzeng@thoughtworks.com>
2020-11-21 09:04:30 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   configureWebpack(config) {
     config.devtool = 'cheap-module-source-map'
+    config.resolve.alias = {
+      vue$: 'vue/dist/vue.esm.js'
+    }
     config.resolve.extensions = ['.vue', '.js', '.jsx', '.json']
     config.devServer = {
       proxy: {
